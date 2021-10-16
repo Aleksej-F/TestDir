f873eacb6fe196b598d1068dc3c9fc6b2527b6c5
AlesejF <l7933@yandex.ru>
2021-10-16 23:37:43 +0300

    исправил меню для моб в

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
+
 	$('.header-burder').click(function(event){
-		$('.burder,.header-burder,.header-menu,.header-row,.header-logo_img,.header-wrap,.header-questions,.header-questions-cloud').toggleClass('active');
-		$('body').toggleClass('lock')
+		const yy = window.pageYOffset
+		
+		window.pageYOffset = y
+		
+		y = yy
+		
+		$('.burder,.header-burder,.header-menu,.header-row,.header-logo_img,.header-wrap,.header-shell,.header-questions,.header-questions-cloud').toggleClass('active');
+		/*$('body').toggleClass('lock')*/
 
 	}); 
+	let y = 0
 	$('.footer-burder').click(function(event){
-		$('.burder,.header-burder,.header-menu,.header-row,.header-logo_img,.header-wrap,.header-questions,.header-questions-cloud').toggleClass('active');
-		$('body').toggleClass('lock')
+		const yy = window.pageYOffset
+		
+		window.pageYOffset = y
+		
+		y = yy
+		
+		$('.burder,.header-burder,.header-menu,.header-row,.header-logo_img,.header-wrap,.header-shell,.header-questions,.header-questions-cloud').toggleClass('active');
+		/*$('body').toggleClass('lock')*/
 
 	}); 
 	$('.the_bell').click(function(event){
